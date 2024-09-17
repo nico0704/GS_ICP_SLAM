adc8c78fb8a8717898f24e0edd9ef8cff1fcb183
Leon Kratz <72967993+leon-kratz@users.noreply.github.com>
2024-09-17 14:55:21 +0200

    Update image_grabber.py

--- docker_folder/image_grabber.py
+++ docker_folder/image_grabber.py
@@ -20,11 +20,16 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 # Start streaming with the chosen resolution and framerate
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start the pipeline
 pipeline.start(config)
+
+# Create an align object
+# rs.stream.color is the stream type we want to align depth frames to
+align_to = rs.stream.color
+align = rs.align(align_to)
 
 i = 0
 index = 0
@@ -32,8 +37,14 @@ try:
     while True:
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
+
+        # Align the depth frame to the color frame
+        aligned_frames = align.process(frames)
+
+        # Get aligned frames
+        depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+
         if not depth_frame or not color_frame:
             print(f"Error: Failed to capture frame {i}.")
             break
@@ -54,9 +65,9 @@ try:
 
         # Save the depth image (scaling to 8-bit for visualization)
         depth_image_path = os.path.join(output_dir, f'depth/depth{index:06}.png')
-        depth_image_8bit = cv2.convertScaleAbs(depth_image, alpha=0.03)
+        depth_image_8bit = cv2.convertScaleAbs(depth_image, alpha=0.2)
+        
         cv2.imwrite(depth_image_path, depth_image_8bit)
-
 
         i += 1
         index += 1
@@ -74,4 +85,3 @@ finally:
 
     # Close any OpenCV windows
     cv2.destroyAllWindows()
-    
