0e6d0ebff8627c9da5d27e8786e820576b716f54
Nico Schröder <82507925+nico0704@users.noreply.github.com>
2024-09-19 23:58:39 +0200

    Update image_grabber.py

--- docker_folder/image_grabber.py
+++ docker_folder/image_grabber.py
@@ -4,84 +4,58 @@ import time
 import numpy as np
 import pyrealsense2 as rs
 from datetime import datetime
-from datetime import date
 import time
 
-# Directory to save the images
-output_dir = f'custom_{date.today().isoformat()}'
+output_dir = "dataset/custom_{}".format(datetime.now().strftime("%d%m%Y_%H_%M"))
 
-# Create the directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
 os.makedirs(f"{output_dir}/rgb", exist_ok=True)
 os.makedirs(f"{output_dir}/depth", exist_ok=True)
 
-# Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
 
-# Start streaming with the chosen resolution and framerate
-config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-# Start the pipeline
 pipeline.start(config)
 
-# Create an align object
-# rs.stream.color is the stream type we want to align depth frames to
 align_to = rs.stream.color
 align = rs.align(align_to)
 
-i = 0
 index = 0
 try:
     while True:
-        # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
-
-        # Align the depth frame to the color frame
         aligned_frames = align.process(frames)
 
-        # Get aligned frames
         depth_frame = aligned_frames.get_depth_frame()
         color_frame = aligned_frames.get_color_frame()
 
         if not depth_frame or not color_frame:
-            print(f"Error: Failed to capture frame {i}.")
+            print(f"Error: Failed to capture frame {index}.")
             break
 
-        # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
-        # Get the current timestamp
         timestamp = time.time()
 
-        print(f"Taking Frame {i}")
+        print(f"Taking Frame {index}")
 
-        # Save the RGB image
-        color_image_filename = f'{i}_color.png'
         color_image_path = os.path.join(output_dir, f'rgb/frame{index:06}.jpg')
         cv2.imwrite(color_image_path, color_image)
-
-        # Save the depth image (scaling to 8-bit for visualization)
         depth_image_path = os.path.join(output_dir, f'depth/depth{index:06}.png')
-        depth_image_8bit = cv2.convertScaleAbs(depth_image, alpha=0.2)
-        
-        cv2.imwrite(depth_image_path, depth_image_8bit)
+        depth_image = cv2.convertScaleAbs(depth_image, alpha=0.03)
+        cv2.imwrite(depth_image_path, depth_image)
 
-        i += 1
         index += 1
-
         time.sleep(0.1)
 
-        # Break the loop if 'q' key is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print("Exiting on user command.")
             break
 
 finally:
-    # Stop the pipeline
     pipeline.stop()
-
-    # Close any OpenCV windows
     cv2.destroyAllWindows()
